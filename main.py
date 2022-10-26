08d79dd4cfc027a0079c05786a9b5058111ae9e2
matyuha-sergej <sergei.matuha@gmail.com>
2022-10-26 22:36:26 +0300

    import lib

---
+++ main.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
